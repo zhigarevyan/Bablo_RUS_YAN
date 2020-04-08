18721b5ad521b7635939d055fbffeea24aa21bb9
Ruslan <kru4kou.work@gmail.com>
2020-04-08 03:03:32 +0300

    Merge branch 'master' of C:\Users\Lenovo\IdeaProjects\Bablo_RUS_YAN with conflicts.

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,13 +1,17 @@
 package entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+//@Data
 @Data
 @RequiredArgsConstructor
 public class Player {
     private int id;
     private String name;
+
 
     public Player(String name) {
         this.name = name;
