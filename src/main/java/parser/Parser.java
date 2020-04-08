ef7d99492b440886e8aaa836bbfd79ebe73bc6de
zhigarevyan <zhigarevyan@users.noreply.github.com>
2020-04-08 03:03:29 +0300

    third

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -11,23 +11,58 @@ import java.io.IOException;
 
 @Data
 public class Parser {
+    private static File file;
 
-    public static void main(String[] args) {
-        final String url ="http://eclipse.org";
-        final String url1 ="https://prognoznado.ru";
-        File file = new File("D:\\UNIVER\\java\\Bablo_RUS_YAN\\src\\main\\resources\\Результаты матчей.html");
-        //final String url = "https://prognoznado.ru/rezultaty-matchej/nastolnyj-tennis/ttstar-serija-chehija-muzhchiny-odinochnyj-razrjad-final/ttstar-serija-chehija-muzhchiny-odinochnyj-razrjad-match-za-3-e-mesto/ttstar-serija-chehija-muzhchiny-odinochnyj-razrjad-1-2-finala/ttstar-serija-chehija-muzhchiny-odinochnyj-razrjad/masters-pro-tur-rossija-muzhchiny-parnyj-razrjad/masters-liga-a-muzhchiny-odinochnyj-razrjad/masters-liga-b-muzhchiny-odinochnyj-razrjad/masters-liga-a-zhenschiny-odinochnyj-razrjad/masters-liga-b-muzhchiny-odinochnyj-razrjad-match-za-3-e-mesto/masters-liga-a-zhenschiny-odinochnyj-razrjad-match-za-3-e-mesto/masters-liga-a-zhenschiny-odinochnyj-razrjad-final/masters-liga-a-muzhchiny-odinochnyj-razrjad-final/masters-liga-a-muzhchiny-odinochnyj-razrjad-match-za-3-e-mesto/masters-liga-b-muzhchiny-odinochnyj-razrjad-final/masters-liga-a-muzhchiny-odinochnyj-razrjad-1-2-finala/masters-liga-c-muzhchiny-odinochnyj-razrjad/kubok-bum-rossija-muzhchiny-odinochnyj-razrjad/masters-liga-c-muzhchiny-odinochnyj-razrjad-final/masters-liga-c-muzhchiny-odinochnyj-razrjad-match-za-3-e-mesto/kubok-bum-rossija-muzhchiny-odinochnyj-razrjad-final/kubok-bum-rossija-muzhchiny-odinochnyj-razrjad-match-za-3-e-mesto/kubok-bum-rossija-muzhchiny-odinochnyj-razrjad-1-2-finala/masters-muzhchiny-odinochnyj-razrjad/masters-mikst/masters-mikst-1-2-finala/masters-mikst-final/masters-mikst-match-za-3-e-mesto.html";
+    public Parser(File file) {
+        this.file = file;
+    }
+
+    void searchScore(){
         Document doc = null;
         try {
             doc = Jsoup.parse(file,"UTF-8");
             Elements form = doc.getElementsByClass("text-success text-right");
             for(Element elem : form){
-                System.out.println(elem);
+                String s = elem.text();
+                System.out.println(s);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    Element searchLine(){
+        try {
+            Elements form = doc.getElementsByClass("text-success text-right");
+            for(Element elem : form){
+                String s = elem.text();
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void searchPlayers(){
+        Document doc = null;
+        try {
+            doc = Jsoup.parse(file,"UTF-8");
+            Elements namesOfPlayers = doc.getElementsByClass("text-success text-right");
+            Elements form = doc.getElementsByClass("small");
+            for(Element elem : form){
+                String s = elem.text();
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        File f = new File("D:\\UNIVER\\java\\Bablo_RUS_YAN\\src\\main\\resources\\Результаты матчей.html");
+        Parser parser = new Parser(f);
+        //parser.searchScore();
+        parser.searchPlayers();
+
     }
 
 }
