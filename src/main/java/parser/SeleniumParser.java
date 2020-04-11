8634e4505dcdf4d07e06452927d3f730c8c90cbe
Ruslan <kru4kou.work@gmail.com>
2020-04-11 20:36:52 +0300

    Бука ебем, или как?

--- src/main/java/parser/SeleniumParser.java
+++ src/main/java/parser/SeleniumParser.java
@@ -4,6 +4,7 @@ import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
 import entity.Player;
 import entity.Result;
 import lombok.SneakyThrows;
+import manager.ServerManager;
 import org.jsoup.nodes.Element;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -14,6 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +24,8 @@ public class SeleniumParser {
     static ArrayList<String> datas = new ArrayList<String>();
     static ArrayList<String> names = new ArrayList<String>();
     static ArrayList<String> results = new ArrayList<String>();
+    static int linesQuantity = 0;
+    static ServerManager serverManager = null;
 
     public static Player[] playersToDBForm(String players){
 
@@ -63,47 +67,64 @@ public class SeleniumParser {
         return results;
     }
 
-    public static void main(String[] args) {
+    public static int getInfoFromWebsite() {
 
-        ParsingThread thread = new ParsingThread();
-        //Player[] players = playersToDBForm("Станислав Ошкин (Рос) - Михаил Леонов (Рос)");
-        thread.run();
-    }
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
-    static class ParsingThread extends Thread {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://1xbet.com/results/");
+        WebDriverWait wait1 = new WebDriverWait(driver, 10000);
+        wait1.until(ExpectedConditions.elementToBeClickable(By.className("c-games__row")));
+        WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"searchGames\"]"));
+        searchBox.sendKeys("Мастерс");
+        List<WebElement> dates= driver.findElements(By.className("c-games__row_light"));
 
-        @SneakyThrows
-        @Override
-        public void run() {
+        int linesQuantity = 0;
+        for (WebElement elements : dates) {
+            String getInput = elements.getAttribute("innerText");
+            //System.out.println(getInput); //SOUT info from WEBElement - Lines.
+            String[] input = getInput.split("\\r?\\n");
 
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://1xbet.com/results/");
-            WebDriverWait wait1 = new WebDriverWait(driver, 10000);
-            wait1.until(ExpectedConditions.elementToBeClickable(By.className("c-games__row")));
-            WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"searchGames\"]"));
-            this.sleep(1000);
-            searchBox.sendKeys("Мастерс");
-            List<WebElement> dates= driver.findElements(By.className("c-games__row_light"));
-
-            int count = 0;
-            for (WebElement elements : dates) {
-                String getInput = elements.getAttribute("innerText");
-                //System.out.println(getInput); //SOUT info from WEBElement - Lines.
-                String[] input = getInput.split("\\r?\\n");
-
-                datas.add(input[0]);
-                names.add(input[1]);
-                results.add(input[2]);
-                count++;
-            }
-
-            System.out.println("Строк отпарсено: "+count);
-//            System.out.println(datas);
-//            System.out.println(names);
-//            System.out.println(results);
-
+            datas.add(input[0]);
+            names.add(input[1]);
+            results.add(input[2]);
+            linesQuantity++;
         }
+        return linesQuantity;
+
     }
+
+    @SneakyThrows
+    public static void insertIntoDB() {
+        serverManager = new ServerManager();
+        Date date;
+        Player[] twoPlayers;
+        Result result;
+        int index;
+        for (index = 0; index < linesQuantity; index++) {
+            date = datasToDB("11.04 07:00");
+            twoPlayers = playersToDBForm(names.get(index));
+            result = scoreToDBForm(results.get(index));
+            serverManager.insertPlayer(twoPlayers[0]);
+            serverManager.insertPlayer(twoPlayers[1]);
+            serverManager.insertResult(result);
+            serverManager.insertMatch(twoPlayers[0],twoPlayers[1],result,date);
+        }
+        System.out.println("Вставлено " + index + " строк.");
+    }
+
+    public static Date datasToDB(String string) {
+        int deleteAfterIndex = string.lastIndexOf(" ");
+        String result = string.substring(0,deleteAfterIndex); //Потом переделать, бо время нужно!
+        String[] tempString= result.split("\\.");
+        return Date.valueOf("2020-"+tempString[1]+'-'+tempString[0]);
+    }
+
+    public static void main(String[] args) {
+        linesQuantity = getInfoFromWebsite(); //returns number of lines;
+        insertIntoDB();
+
+
+    }
+
 }
