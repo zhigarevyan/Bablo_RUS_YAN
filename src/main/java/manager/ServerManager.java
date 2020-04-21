04f1796d14dd8f21d56a4ccca15489d46a2a8a4d
Ruslan <kru4kou.work@gmail.com>
2020-04-21 15:32:55 +0300

    работаем

--- src/main/java/manager/ServerManager.java
+++ src/main/java/manager/ServerManager.java
@@ -186,7 +186,6 @@ public class ServerManager {
                 }
 
                 setScores = Arrays.copyOfRange(resultArray, 4, 11);
-                double[] forasAndTotals = getForaAndTotals(setScores);
                 countMatches++;
 
 
@@ -200,11 +199,14 @@ public class ServerManager {
 
 
                 if (rs.getString(2).equals(name)) {
+                    double[] forasAndTotals = getForaAndTotals(setScores);
                     System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f | %s %s %s %s %s %s %s \n", resultArray[0], resultArray[1], resultArray[2], result[0], result[1], forasAndTotals[0], forasAndTotals[2], forasAndTotals[1], forasAndTotals[3], forasAndTotals[2] + forasAndTotals[3], setScores[0], setScores[1], setScores[2], setScores[3], setScores[4], setScores[5], setScores[6]);
                     //не закочено!
+
                     //System.out.printf("%s | %s - %s | %s : %s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f\n", resultArray[0],resultArray[1],resultArray[2],result[0],result[1],forasAndTotals[0],forasAndTotals[2],forasAndTotals[1],forasAndTotals[3], forasAndTotals[2]+forasAndTotals[3]);
                 } else {
                     setScores = reverseSets(setScores);
+                    double[] forasAndTotals = getForaAndTotals(setScores);
                     //System.out.printf("%s | %s - %s | %s : %s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f\n", resultArray[0],resultArray[2],resultArray[1],result[1],result[0],forasAndTotals[1],forasAndTotals[3],forasAndTotals[0],forasAndTotals[2], forasAndTotals[2]+forasAndTotals[3]);
                     //не закочено!
                     System.out.printf("%s | %s - %s | %s : %s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f | %s %s %s %s %s %s %s\n", resultArray[0], resultArray[2], resultArray[1], result[1], result[0], forasAndTotals[1], forasAndTotals[3], forasAndTotals[0], forasAndTotals[2], forasAndTotals[2] + forasAndTotals[3], setScores[0], setScores[1], setScores[2], setScores[3], setScores[4], setScores[5], setScores[6]);
@@ -287,7 +289,6 @@ public class ServerManager {
                 }
 
                 setScores = Arrays.copyOfRange(resultArray, 4, 11);
-                double[] forasAndTotals = getForaAndTotals(setScores);
 
                 if (Double.valueOf(result[0]) > Double.valueOf(result[1])) {
                     if (rs.getString(2).equals(name1))
@@ -302,15 +303,17 @@ public class ServerManager {
                         winsOfPlayer1++;
                 }
 
-                if (rs.getString(2).equals(name1))
+                if (rs.getString(2).equals(name1)) {
                     //not finished
+                    double[] forasAndTotals = getForaAndTotals(setScores);
                     //System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f\n", resultArray[0],resultArray[1],resultArray[2],result[0],result[1],forasAndTotals[0],forasAndTotals[2],forasAndTotals[1],forasAndTotals[3], forasAndTotals[2]+forasAndTotals[3]);
                     System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f | %s %s %s %s %s %s %s \n", resultArray[0], resultArray[1], resultArray[2], result[0], result[1], forasAndTotals[0], forasAndTotals[2], forasAndTotals[1], forasAndTotals[3], forasAndTotals[2] + forasAndTotals[3], setScores[0], setScores[1], setScores[2], setScores[3], setScores[4], setScores[5], setScores[6]);
-
+                }
                 else {
                     setScores = reverseSets(setScores);
+                    double[] forasAndTotals = getForaAndTotals(setScores);
                     //not finished
-                    System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f | %s %s %s %s %s %s %s \n", resultArray[0], resultArray[2], resultArray[1], result[1], result[0], forasAndTotals[0], forasAndTotals[2], forasAndTotals[2], forasAndTotals[1], forasAndTotals[2] + forasAndTotals[3], setScores[0], setScores[1], setScores[2], setScores[3], setScores[4], setScores[5], setScores[6]);
+                    System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f | %s %s %s %s %s %s %s \n", resultArray[0], resultArray[2], resultArray[1], result[1], result[0], forasAndTotals[1], forasAndTotals[3], forasAndTotals[0], forasAndTotals[2], forasAndTotals[2] + forasAndTotals[3], setScores[0], setScores[1], setScores[2], setScores[3], setScores[4], setScores[5], setScores[6]);
                     //System.out.printf("%s | %s - %s | %s:%s | Fora1: %.0f Total1: %.0f | Fora2: %.0f Total2: %.0f | Total: %.0f\n", resultArray[0],resultArray[2],resultArray[1],result[1],result[0],forasAndTotals[1],forasAndTotals[3],forasAndTotals[0],forasAndTotals[2], forasAndTotals[2]+forasAndTotals[3]);
                 }
 
@@ -392,8 +395,8 @@ public class ServerManager {
             //serverManager.insertResult(result);
             //serverManager.insertResult(result1);
             //serverManager.insertMatch(player1,player,result,date);
-            String p1 = "Илья Нашивочников";
-                String p2 = "Максим Хмелевский";
+            String p1 = "Олег Косых";
+                String p2 = "Андрей Кондрашкин";
 
             System.out.printf("\n -------------%s statistics------------- \n", p1);
             serverManager.searchPlayersMatchWithForaAndTotals(p1);
